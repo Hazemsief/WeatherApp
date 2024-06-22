5180f944cbd1b55b5e918da4bb8910ff447c22cc
Hazemsief <Hazemsief17@gmail.com>
2024-06-22 17:43:15 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,162 @@
+const navLinks = Array.from(document.querySelectorAll('.nav-item'));
+const btnFind = document.querySelector('#btnFind');
+const findInput = document.querySelector('#findInput');
+const forecast = document.querySelector('#forecast');
+
+const APIKey = '0504276985d1463eaa6155223233112';
+const baseURL = 'https://api.weatherapi.com/v1/forecast.json';
+
+let searchLocation = '';
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+getUserCity();
+
+function toggleCssClass(el, className, condition) {
+    condition ? el.classList.add(className) : el.classList.remove(className);
+}
+
+function formatDate(dateString, format) {
+    const d = new Date(dateString);
+    return format === 'day' ? days[d.getDay()] : monthNames[d.getMonth()];
+}
+
+function normalizeCityName(cityName) {
+    return cityName.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function getUserCity() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const { latitude, longitude } = position.coords;
+            try {
+                const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`);
+                const data = await response.json();
+                searchLocation = normalizeCityName(data.city || 'Alex');
+                displayWeather();
+            } catch {
+                searchLocation = 'Alex';
+                displayWeather();
+            }
+        }, () => {
+            searchLocation = 'Alex';
+            displayWeather();
+        });
+    } else {
+        searchLocation = 'Alex';
+        displayWeather();
+    }
+}
+
+function handleActiveNavLink(linksArr, link) {
+    linksArr.forEach(l => toggleCssClass(l, 'active', false));
+    toggleCssClass(link, 'active', true);
+}
+
+async function getWeather(searchLocation) {
+    try {
+        const response = await fetch(`${baseURL}?key=${APIKey}&q=${searchLocation}&days=3`);
+        return response.ok ? await response.json() : null;
+    } catch {
+        return null;
+    }
+}
+
+async function displayWeather() {
+    const weatherList = await getWeather(searchLocation);
+    if (!weatherList) return;
+
+    const { current: currentObj, location: locationObj, forecast: { forecastday: forecastArr } } = weatherList;
+
+    const weatherContainer = `
+        <!-- Today -->
+        <div class="col-lg-4">
+            <div class="forecast today rounded-3">
+                <div id="today" class="forecast-header px-3 d-flex justify-content-between">
+                    <div class="day fw-semibold">${formatDate(locationObj.localtime, 'day')}</div>
+                    <div class="date text-light">${new Date(locationObj.localtime).getDate()} ${formatDate(locationObj.localtime, 'month')}</div>
+                </div>
+                <div id="current" class="forecast-content px-3">
+                    <div class="location fs-4">${locationObj.country}, ${locationObj.name}</div>
+                    <div class="time me-auto">update: ${locationObj.localtime.split(' ')[1]}</div>
+                    <div class="degree d-flex justify-content-between mt-2">
+                        <div class="num">${currentObj.temp_c}<sup>o</sup>C</div>
+                        <div class="forecast-icon align-self-end">
+                            <img src=${currentObj.condition.icon} alt="" width="90">
+                        </div>
+                    </div>
+                    <p class="custom fs-5">${currentObj.condition.text}</p>
+                    <div class="mt-4 d-flex justify-content-between">
+                        <span class="fs-5"><img class="me-2" src="./imgs/icon-compass.png"" alt="">${currentObj.wind_degree}%</span>
+                        <span class="fs-5"><img class="me-2" src="./imgs/icon-umberella.png" alt="">${currentObj.wind_kph}km/h</span>
+                        <span class="fs-5"><img class="me-2" src="./imgs/icon-wind.png" alt="">${currentObj.wind_dir}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Tomorrow -->
+        <div id="tomorrow" class="col-lg-4 mt-3 mt-lg-0">
+            <div class="text-center h-100 rounded-2 forecast tomorrow">
+                <div class="forecast-header fw-semibold px-3">
+                    <div class="day">${formatDate(forecastArr[1].date, 'day')}</div>
+                </div>
+                <div class="forecast-content px-3">
+                    <div class="forecast-icon">
+                        <i class="fa-solid fa-sun weather-icon"></i>
+                    </div>
+                    <div class="degree fs-2">${forecastArr[1].day.maxtemp_c}<sup>o</sup>C</div>
+                    <p class="fs-5 mb-0">${forecastArr[1].day.mintemp_c}<sup>o</sup></p>
+                    <p class="custom fs-5">${forecastArr[1].day.condition.text}</p>
+                    <div class="mt-4 d-flex justify-content-between">
+                        <span class="fs-5"><img class="me-2" src="./imgs/icon-compass.png"" alt="">${currentObj.wind_degree}%</span>
+                        <span class="fs-5"><img class="me-2" src="./imgs/icon-umberella.png" alt="">${currentObj.wind_kph}km/h</span>
+                        <span class="fs-5"><img class="me-2" src="./imgs/icon-wind.png" alt="">${currentObj.wind_dir}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- After Tomorrow -->
+        <div id="afterTomorrow" class="col-lg-4 mt-3 mt-lg-0">
+            <div class="text-center h-100 rounded-2 forecast">
+                <div class="forecast-header fw-semibold px-3">
+                    <div class="day">${formatDate(forecastArr[2].date, 'day')}</div>
+                </div>
+                <div class="forecast-content px-3">
+                    <div class="forecast-icon">
+                        <i class="fa-solid fa-sun weather-icon"></i>
+                    </div>
+                    <div class="degree fs-2">${forecastArr[2].day.maxtemp_c}<sup>o</sup>C</div>
+                    <p class="fs-5 mb-0">${forecastArr[2].day.mintemp_c}<sup>o</sup></p>
+                    <p class="custom fs-5">${forecastArr[2].day.condition.text}</p>
+                    <div class="mt-4 d-flex justify-content-between">
+                    <span class="fs-5"><img class="me-2" src="./imgs/icon-compass.png"" alt="">${currentObj.wind_degree}%</span>
+                    <span class="fs-5"><img class="me-2" src="./imgs/icon-umberella.png" alt="">${currentObj.wind_kph}km/h</span>
+                    <span class="fs-5"><img class="me-2" src="./imgs/icon-wind.png" alt="">${currentObj.wind_dir}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    forecast.innerHTML = weatherContainer;
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('click', e => handleActiveNavLink(navLinks, e.target.parentNode));
+});
+
+const handleFind = () => {
+    if (findInput.value) {
+        searchLocation = findInput.value;
+        displayWeather();
+    }
+}
+
+btnFind.addEventListener('click', e => {
+    e.preventDefault();
+    handleFind();
+});
+
+findInput.addEventListener('keyup', e => {
+    e.preventDefault();
+    handleFind();
+});
